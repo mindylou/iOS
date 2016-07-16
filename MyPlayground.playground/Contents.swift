6dfad1a33c43e5b99fab760f523ed83c8e68dd74
Mindy Lou <mml234@cornell.edu>
2016-07-15 22:50:52 -0400

    app dev intro
    Trying to learn basics of xcode  and Swift

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var message = "Hello world!"
+print(message)
+
+var a = 1
+var b = 15
+var total = a+b
+print(total)
+
+if(total < 20){
+    print("Hello")
+}
---
+++ war/warTests/warTests.swift
@@ -0,0 +1,36 @@
+//
+//  warTests.swift
+//  warTests
+//
+//  Created by Mindy Lou on 7/15/16.
+//  Copyright © 2016 Mindy Lou. All rights reserved.
+//
+
+import XCTest
+@testable import war
+
+class warTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
