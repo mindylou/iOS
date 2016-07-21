56b8eb3d862cc969eea8add3858f64c0e47ec649
Mindy Lou <mml234@cornell.edu>
2016-07-20 21:40:12 -0400

    Assets and card randomization

---
+++ RandomPlayground.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// arc4random: imported from UIKit, generates a random number between 0, #-1
+var random_number = arc4random_uniform(13) + 1 // random number from 1-13
+
+// adding Strings:
+var some_text = "balloon"
+String(format: "te%@st", some_text)
+
+// adding Strings to ints:
+String(format: "card%i", random_number)
+
+// Arrays: all have to be same type
+var my_array:[String] = ["book", "cat"]
+
+// length of array
+my_array.count
+
+// inserting new elements into array
+my_array.insert("dog", atIndex: 0)
+my_array.append("dolphin")
+my_array += ["elephant", "truck"]
+
+my_array.removeAtIndex(1)
+
+// creating a new array object
+var int_array:[Int] = [Int]()
+int_array.append(0)
